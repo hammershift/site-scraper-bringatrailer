e1ab8e9e40e3bb29f5a3d56435454409ca2214c5
Amiel Brencis Salipande <absalipande@gmail.com>
2024-02-01 21:40:25 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+module.exports = {
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.map$/,
+      use: ['ignore-loader'],
+    });
+
+    return config;
+  },
+};
